9838acde73f345ab0487cdd96ef6d0ceb45b756a
default <default@email.com>
2024-05-13 18:43:57 +0300

    Make migrations, change CORS config, add Dockerfile

--- dist/src/main.js
+++ dist/src/main.js
@@ -4,7 +4,15 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            `https://${process.env.DOMAIN}`,
+            `https://${process.env.FRONTEND_DOMAIN}`,
+        ],
+        allowedHeaders: [],
+        credentials: true,
+        methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE", "PATCH"],
+    });
     app.setGlobalPrefix('api');
     app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+	origin: [
+		`https://${process.env.DOMAIN}`,
+		`https://${process.env.FRONTEND_DOMAIN}`,
+
+
+	],
+	allowedHeaders: [],
+	credentials: true,
+	methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE", "PATCH"],
+  })
   app.setGlobalPrefix('api')
   app.listen(3000);
 }
