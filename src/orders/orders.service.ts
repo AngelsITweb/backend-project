dfa79520b6d4bb9420dbf119debb26864ec8584b
siqq <144137886+siqqdev@users.noreply.github.com>
2024-05-30 12:40:18 +0200

    Update orders.service.ts

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -113,7 +113,7 @@ export class OrdersService {
 
         const message = `Новый заказ на ${cart.parts.length} деталей`;
         const promises = managers.map(async (manager) => {
-            await this.botService.sendMessage(manager.telegramId, message, 'https://mygarage-webapp-nawq6gs1x-ceos-projects-828a268d.vercel.app/manager/actual-orders');
+            await this.botService.sendMessage(manager.telegramId, message, 'https://mygarage-webapp.vercel.app/manager/actual-orders');
         });
         await Promise.all(promises);
         return order;
