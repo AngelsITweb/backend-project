47724375a3476a86fb36fd247e93258be0bcc8ac
AngelsITweb <147707018+AngelsITweb@users.noreply.github.com>
2024-06-20 16:24:19 +0500

    Update image.service.ts

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import * as path from "path";
-import * as fs from "fs";
+import { Injectable, BadRequestException } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class ImageService {
-    async uploadImage(file: any): Promise<string> {
+    async uploadImage(file: any): Promise<string | null> {
+        if (!file) {
+            // Если файл не был загружен, возвращаем null или другой подходящий ответ
+            return null;
+        }
+
         const timestamp = Date.now();
         const random = Math.floor(Math.random() * 1000);
         const filename = `${timestamp}-${random}-${file.originalname}`;
