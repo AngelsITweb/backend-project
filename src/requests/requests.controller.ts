7dbd89039bae1003c29043ee50684c1abc4711b9
AngelsITweb <147707018+AngelsITweb@users.noreply.github.com>
2024-08-28 15:44:43 +0500

    Update requests.controller.ts

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -20,15 +20,15 @@ export class RequestController {
     }
 
     @Post('')
-    async createRequest(
-        @Body() body: any,
-        @Headers('user-id') userId: string
-    ): Promise<any> {
-        const parsedUserId = parseInt(userId, 10);
-        const { carId, name, image }: { carId: string; name: string, image: string } = body;
-        const parsedCarId = parseInt(carId, 10);
-        return this.requestsService.createRequest({ userId: parsedUserId, carId: parsedCarId, name, image });
-    }
+  async createRequest(
+    @Body() body: any,
+    @Headers('user-id') userId: string
+  ): Promise<any> {
+    const parsedUserId = parseInt(userId, 10);
+    const { carId, name, image }: { carId: string; name: string; image?: string } = body;
+    const parsedCarId = parseInt(carId, 10);
+    return this.requestsService.createRequest({ userId: parsedUserId, carId: parsedCarId, name, image });
+  }
 
     @Get('/notifications')
     getByNotifications(@Headers('user-id') userId: string) {
