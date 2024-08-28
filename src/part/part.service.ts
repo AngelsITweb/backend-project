9e12a8b40f2ce564584f6cd5f90eea932272cb83
AngelsITweb <147707018+AngelsITweb@users.noreply.github.com>
2024-08-28 15:50:42 +0500

    Update part.service.ts

--- src/part/part.service.ts
+++ src/part/part.service.ts
@@ -1,40 +1,57 @@
-import { Injectable } from '@nestjs/common';
-import {PrismaService} from "../../prisma/prisma.service";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from "../../prisma/prisma.service";
 
 @Injectable()
 export class PartService {
-    constructor(private readonly prisma: PrismaService ) {}
+    constructor(private readonly prisma: PrismaService) {}
 
-    getAll(carId: number) {
+    async getAll(carId: number) {
         return this.prisma.part.findMany({
             where: {
                 carId
             }
-        })
+        });
     }
 
-    getById(id: number) {
-        return this.prisma.part.findUnique({
+    async getById(id: number) {
+        const part = await this.prisma.part.findUnique({
             where: {
                 id
             }
-        })
+        });
+
+        if (!part) {
+            throw new NotFoundException(`Part with ID ${id} not found`);
+        }
+
+        return part;
     }
 
-    createPart(requestId: number, name: string, isNew: boolean, isOriginal: boolean, manufacturer: string, numberOrName: string, price: number, image?: string, sellerId: number, carId: number) {
-    return this.prisma.part.create({
-      data: {
-        name,
-        new: isNew,
-        original: isOriginal,
-        manufacturer,
-        numberOrName,
-        price,
-        image: image || null,
-        sellerId,
-        carId,
-        requestId
-      }
-    })
-  }
+    async createPart(
+        requestId: number,
+        name: string,
+        isNew: boolean,
+        isOriginal: boolean,
+        manufacturer: string,
+        numberOrName: string,
+        price: number,
+        image: string | undefined,
+        sellerId: number,
+        carId: number
+    ) {
+        return this.prisma.part.create({
+            data: {
+                name,
+                new: isNew,
+                original: isOriginal,
+                manufacturer,
+                numberOrName,
+                price,
+                image: image || null,
+                sellerId,
+                carId,
+                requestId
+            }
+        });
+    }
 }
