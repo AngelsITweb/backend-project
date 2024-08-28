a82164ca66a3a3d19a0a0ad56d2f42ca5b9e0f7c
AngelsITweb <147707018+AngelsITweb@users.noreply.github.com>
2024-08-28 15:34:23 +0500

    Update image.controller.ts

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UploadedFile, UseInterceptors, Res } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, Body, Res } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ImageService } from './image.service';
 import { Response } from 'express';
@@ -14,16 +14,16 @@ export class ImageController {
 
   @Post('')
   @UseInterceptors(FileInterceptor('image'))
-  async uploadImage(@UploadedFile() image: UploadedFile | undefined, @Res() res: Response): Promise<any> {
+  async uploadImage(
+    @UploadedFile() image: UploadedFile | undefined,
+    @Body() body: any,
+    @Res() res: Response
+  ): Promise<any> {
     try {
-      const result = await this.imageService.uploadImage(image);
-      if (result) {
-        return res.status(200).json(result);
-      } else {
-        return res.status(200).json({ message: 'Файл не был загружен' });
-      }
+      const result = await this.imageService.processRequest(image, body);
+      return res.status(200).json(result);
     } catch (error) {
-      return res.status(500).json({ message: 'Ошибка при загрузке файла', error: error.message });
+      return res.status(500).json({ message: 'Ошибка при обработке запроса', error: error.message });
     }
   }
 }
